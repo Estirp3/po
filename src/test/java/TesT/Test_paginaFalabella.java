bf28037b82e3ceac1202a5a90665eab6bce056b5
Estirp3 <patricio.calderonig@gmail.com>
2019-12-01 05:08:15 -0300

    se sube

---
+++ src/test/java/TesT/Test_paginaFalabella.java
@@ -0,0 +1,59 @@
+package Test;
+
+import Pobjetc.BasePage;
+import Pobjetc.PaginaInicio;
+import Pobjetc.ProductoPO;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.BasicConfigurator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class Test_paginaFalabella {
+    WebDriver driver;
+    PaginaInicio pa;
+    ProductoPO prod;
+    BasePage base;
+
+
+    @Before
+    public void inicio(){
+        BasicConfigurator.configure();// log4g
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.falabella.com/falabella-cl/");
+        //this.driver = new BrowserFactory.getBrowser();
+        this.pa = new PaginaInicio(this.driver);
+        this.prod = new ProductoPO(driver);
+        this.base = new BasePage(driver);
+    }
+
+
+
+    @Test
+    public void test_01() {
+        pa.inicio();
+        prod.BuscarProducto();
+        prod.valores();
+        prod.carro();
+    }
+
+    @Test
+    public void Test_02(){
+        pa.menuInicio();
+        prod.BuscarProducto();
+        prod.valores();
+        prod.carroObsiones();
+
+    }
+
+    @After
+    public void cerrar(){
+        driver.close();
+    }
+}
